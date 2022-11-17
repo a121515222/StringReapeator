bd96476d1ac5661de1a90c84d3d60220f473be3b
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-11-17 14:03:06 +0800

    first commit

---
+++ String_repeat/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace StringReapeator
+{
+    class StringRepeat
+    {
+        public string RepeatString(string data)
+        {
+            int stringLen = data.Length;
+            string result = "";
+            for (var i = 0; i < stringLen; i++)
+            {
+                result += data;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ String_repeat/Run.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StringReapeator;
+namespace Kata
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringRepeat action = new StringRepeat();
+            Console.WriteLine(action.RepeatString("I'Jack"));
+        }
+    }
+}
